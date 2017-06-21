6f25794631801be3e8e943ba76509de1528dc5cf
Marquavious Draggon <marq.draggon@gmail.com>
2017-06-21 19:35:12 -0400

    updated the updater time

--- networking_test/AppDelegate.swift
+++ networking_test/AppDelegate.swift
@@ -16,14 +16,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        DataManager.sharedInstance.checkDateToUpdateJSON(10)
+        DataManager.sharedInstance.checkDateToUpdateJSON(30)
         return true
     }
 
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
-        DataManager.sharedInstance.checkDateToUpdateJSON(10)
+        DataManager.sharedInstance.checkDateToUpdateJSON(30)
     }
 
     func applicationDidEnterBackground(_ application: UIApplication) {
@@ -33,12 +33,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationWillEnterForeground(_ application: UIApplication) {
         // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the 
-       DataManager.sharedInstance.checkDateToUpdateJSON(10)
+       DataManager.sharedInstance.checkDateToUpdateJSON(30)
     }
 
     func applicationDidBecomeActive(_ application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-        DataManager.sharedInstance.checkDateToUpdateJSON(10)
+        DataManager.sharedInstance.checkDateToUpdateJSON(30)
     }
 
     func applicationWillTerminate(_ application: UIApplication) {
